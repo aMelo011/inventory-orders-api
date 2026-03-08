6b69044f377123ec8b7f76c0afb98c7e43ac06ed
aMelo011 <icaromelo011@gmail.com>
2026-03-08 11:51:47 +0000

    feat: add AppUser entity, migration and repository

---
+++ apps/inventory-api/src/main/java/com/melo/inventory/repository/AppUserRepository.java
@@ -0,0 +1,10 @@
+package com.melo.inventory.repository;
+
+import com.melo.inventory.model.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+    Optional<AppUser> findByEmail(String email);
+}
